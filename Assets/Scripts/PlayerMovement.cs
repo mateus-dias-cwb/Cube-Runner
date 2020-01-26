d30918bb7820974821d40e2744848545bb0e6bef
Mateus Dias <ckateusdias@gmail.com>
2020-01-26 04:46:44 -0300

    Gameplay, Obstáculos e Pontuação

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,31 +2,23 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 1000f;
-    //public Transform playerTrans;
-    //public float expRadius, explosionForce;
+  public Rigidbody rb;
+  public float forwardForce = 2000f;
+  public float sidewaysForce = 1000f;
 
-    // Start is called before the first frame update
-    void Start()
+  // FixedUpdate é preferível ao invés de Update para tratar física
+  void FixedUpdate()
+  {
+    //AddForce é uma função para objetos do tipo RigidBodys
+    rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Adiciona uma força constante para "frente"
+
+    if (Input.GetKey("d"))
     {
-        //rb.useGravity = false;
-        //rb.AddExplosionForce(explosionForce, playerTrans.position, expRadius);
+      rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
     }
-
-    // Update is called once per frame
-    // FixedUpdate é preferível ao invés de Update para tratar física
-    void FixedUpdate()
+    if (Input.GetKey("a"))
     {
-        //AddForce é uma função para objetos do tipo RigidBodys
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-        if (Input.GetKey("d")) {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey("a")) {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-        }
+      rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
     }
+  }
 }
